51651dfa9b6bf3e94ab7fb033405942b520e9a3d
HFラボ <hf.labo.free@gmail.com>
2022-09-04 15:20:48 +0900

    Initial commit

---
+++ MfcGdiDoubleBuffering/MfcGdiDoubleBufferingDlg.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "MfcGdiDoubleBuffering.h"
+#include "MfcGdiDoubleBufferingDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CMfcGdiDoubleBufferingDlg::CMfcGdiDoubleBufferingDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCGDIDOUBLEBUFFERING_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMfcGdiDoubleBufferingDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMfcGdiDoubleBufferingDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+BOOL CMfcGdiDoubleBufferingDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	SetIcon(m_hIcon, TRUE);
+	SetIcon(m_hIcon, FALSE);
+
+	return TRUE;
+}
+
+void CMfcGdiDoubleBufferingDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this);
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+HCURSOR CMfcGdiDoubleBufferingDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ MfcGdiDoubleBuffering/MfcGdiDoubleBufferingDlg.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+class CMfcGdiDoubleBufferingDlg : public CDialogEx
+{
+public:
+	CMfcGdiDoubleBufferingDlg(CWnd* pParent = nullptr);
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCGDIDOUBLEBUFFERING_DIALOG };
+#endif
+
+protected:
+	HICON m_hIcon;
+
+	virtual void DoDataExchange(CDataExchange* pDX);
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
